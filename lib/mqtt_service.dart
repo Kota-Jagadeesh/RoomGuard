f1bc7eee31e04f457d09e1fe3a95086533322e68
Kota-Jagadeesh <kota.jagadesh123@gmail.com>
2026-05-06 17:18:16 +0530

    feat: setup mqtt service

--- lib/mqtt_service.dart
+++ lib/mqtt_service.dart
@@ -1,1 +1,32 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
+class MQTTService {
+  late MqttServerClient client;
+
+  Future<void> connect() async {
+    client = MqttServerClient('broker.hivemq.com', 'flutter_client');
+
+    client.port = 1883;
+
+    client.keepAlivePeriod = 20;
+
+    try {
+      await client.connect();
+
+      print("MQTT Connected");
+    } catch (e) {
+      print("MQTT Connection Failed");
+
+      print(e);
+    }
+  }
+
+  void publishMessage(String topic, String message) {
+    final builder = MqttClientPayloadBuilder();
+
+    builder.addString(message);
+
+    client.publishMessage(topic, MqttQos.atMostOnce, builder.payload!);
+  }
+}
